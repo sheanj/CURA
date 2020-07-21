5da85b8f18dabea29571f5c305035cf6662a4719
Shean <csheanjohnson@gmail.com>
2020-07-21 12:16:31 -0400

    adjusted api-helper file

--- client/src/Services/api-helper.js
+++ client/src/Services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://school-app-test.herokuapp.com/' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://cura-api.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
