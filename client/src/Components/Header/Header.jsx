4296d848c0f4af0d731b54691de1f0ba65735127
Shean <csheanjohnson@gmail.com>
2020-07-21 21:10:34 -0400

    adjusted header link to route to timeline component when user logged in

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -5,10 +5,11 @@ import '../Header/Header.css';
 
 export default class Header extends Component {
   render() {
-    // if (this.props.loggedUser)
     return (
       <header>
-        <Link to='/'><img src={Logo} /></Link>
+        {localStorage.getItem("authToken") ?
+          (<Link to='/timeline'><img src={Logo} /></Link>) :
+          (<Link to='/'><img src={Logo} /></Link>)}
         {this.props.loggedUser ?
           <button onClick={this.props.handleLogout}>LOGOUT</button>
           :
