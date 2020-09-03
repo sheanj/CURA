4e9bff32a1d86a7ad3ded2b113f9549ff941a3b2
Shean Johnson <csheanjohnson@gmail.com>
2020-09-03 17:33:12 -0400

    Header component refactor, implemented button styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter, Route } from "react-router-dom";
+import './App.css'
 
 import {
   loginUser,
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
 import Logo from '../../Assets/Logo/CURA.png';
 import '../Header/Header.css';
 
 export default class Header extends Component {
   render() {
     return (
+      // <header>
+      //   {localStorage.getItem("authToken") ?
+      //     (<Link to='/timeline'><img src={Logo} /></Link>) :
+      //     (<Link to='/'><img src={Logo} /></Link>)}
+      //   {this.props.loggedUser ?
+      //     <button onClick={this.props.handleLogout}>LOGOUT</button>
+      //     :
+      //     <>
+      //       <button onClick={this.props.handlePopOver}>Log In</button>
+      //       <button onClick={this.props.registerPopOver}>Register</button>
+      //     </>
+      //   }
+      // </header>
       <header>
+        <Route path="/" exact><button onClick={this.props.registerPopOver}>REGISTER</button></Route>
         {localStorage.getItem("authToken") ?
           (<Link to='/timeline'><img src={Logo} /></Link>) :
           (<Link to='/'><img src={Logo} /></Link>)}
@@ -14,8 +28,7 @@ export default class Header extends Component {
           <button onClick={this.props.handleLogout}>LOGOUT</button>
           :
           <>
-            <button onClick={this.props.handlePopOver}>Log In</button>
-            <button onClick={this.props.registerPopOver}>Register</button>
+            <button onClick={this.props.handlePopOver}>LOGIN</button>
           </>
         }
       </header>
