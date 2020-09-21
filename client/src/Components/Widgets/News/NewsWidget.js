5da286d1938173dc406c21522047acd1a6abde35
Shean Johnson <csheanjohnson@gmail.com>
2020-09-21 14:48:43 -0400

    adjusted hover color on calendar widget

--- client/src/Components/Widgets/News/NewsWidget.js
+++ client/src/Components/Widgets/News/NewsWidget.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export const getArticles = async () => {
     let key = process.env.REACT_APP_NEWS_WIDGET_API_TOKEN;
-    console.log(process.env.REACT_APP_NEWS_WIDGET_API_TOKEN)
     const response = await axios.get(`https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${key}`);
     let articles = response.data.results;
     return articles
